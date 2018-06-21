840b56f5fc6e3ac9a8583ee54038e0e20a4bdb2c
Gabriele Barbieri <gabriele.barbieri@qucit.com>
2018-06-21 15:05:28 +0200

    try an other fix

--- abletonlink/__init__.py
+++ abletonlink/__init__.py
@@ -6,9 +6,5 @@ __author__ = """Gabriele Barbieri"""
 __email__ = 'gabriele.barbieri83@gmail.com'
 __version__ = '0.1.0'
 
-try:
-    from .abletonlink import Link
-    __ALL__ = [Link]
-except ImportError as e:
-    print('No module named abletonlink')
-    print('Are you sure that you built it?')
+from .abletonlink import Link
+__ALL__ = [Link]
