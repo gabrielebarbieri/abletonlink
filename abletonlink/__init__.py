0b7cb7a05cec74f98b1b42abba2840051065994f
Gabriele Barbieri <gabriele.barbieri@qucit.com>
2018-06-21 15:07:36 +0200

    Revert "try an other fix"
    This reverts commit 840b56f5fc6e3ac9a8583ee54038e0e20a4bdb2c.

--- abletonlink/__init__.py
+++ abletonlink/__init__.py
@@ -6,5 +6,9 @@ __author__ = """Gabriele Barbieri"""
 __email__ = 'gabriele.barbieri83@gmail.com'
 __version__ = '0.1.0'
 
-from .abletonlink import Link
-__ALL__ = [Link]
+try:
+    from .abletonlink import Link
+    __ALL__ = [Link]
+except ImportError as e:
+    print('No module named abletonlink')
+    print('Are you sure that you built it?')
